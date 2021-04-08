8abaf5c371bf802330819f346354bb342d758797
watayouxiang <watayouxiang@qq.com>
2021-04-08 17:57:37 +0800

    empty

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AndroidFrame
+
+for android practice
+
+- https://www.yuque.com/books/share/e5229bf0-07e9-47d5-81a8-49c5594ca22d
+
---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,12 @@
+class Config {
+
+    static applicationId = 'com.watayouxiang.myapplication'
+    static appName = 'AndroidFrame'
+
+    static compileSdkVersion = 27
+    static minSdkVersion = 16
+    static targetSdkVersion = 30
+    static versionCode = 1_000_000
+    static versionName = '1.0.0'
+
+}
