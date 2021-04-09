4e4c4205cafbd9286510195fd69fe979208133f0
watayouxiang <watayouxiang@qq.com>
2021-04-09 17:02:22 +0800

    empty

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -13,20 +13,19 @@ class Config {
     static leakcanary_version = '1.6.3'
 
     static depConfig = [
-            kotlin       : "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version",
-            material     : "com.google.android.material:material:1.2.1",
             androidx     : [
                     core_ktx        : "androidx.core:core-ktx:1.3.2",
                     appcompat       : "androidx.appcompat:appcompat:1.2.0",
                     multidex        : "androidx.multidex:multidex:2.0.0",
                     constraintlayout: "androidx.constraintlayout:constraintlayout:2.0.4",
             ],
+            kotlin       : "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version",
+            material     : "com.google.android.material:material:1.2.1",
             leakcanary   : [
                     android         : "com.squareup.leakcanary:leakcanary-android:$leakcanary_version",
                     android_no_op   : "com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version",
                     support_fragment: "com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version",
             ],
-
             utilcode     : "com.blankj:utilcodex:1.30.5",
             // 混淆
             free_proguard: "com.blankj:free-proguard:1.0.1",
@@ -34,4 +33,52 @@ class Config {
             swipe_panel  : "com.blankj:swipe-panel:1.1",
     ]
 
+    // =============================================================================================
+    // 新的方式
+    // =============================================================================================
+
+    // appConfig 配置的是可以跑 app 的模块，git 提交务必只包含 launcher
+    static appConfig = ['launcher', 'feature0']
+    // pkgConfig 配置的是要依赖的功能包，为空则依赖全部，git 提交务必为空
+    static pkgConfig = ['feature0']
+    static _depConfig = [
+            feature      : [
+                    launcher: [
+                            app: new DepConfig(":feature:launcher:app"),
+                    ],
+                    feature0: [
+                            app   : new DepConfig(":feature:feature0:app"),
+                            pkg   : new DepConfig(true, ":feature:feature0:pkg", "com.blankj:feature-feature0-pkg:1.0", true),
+                            export: new DepConfig(":feature:feature0:export"),
+                    ],
+                    feature1: [
+                            app   : new DepConfig(":feature:feature1:app"),
+                            pkg   : new DepConfig(":feature:feature1:pkg"),
+                            export: new DepConfig(":feature:feature1:export"),
+                    ],
+            ],
+            lib          : [
+                    base  : new DepConfig(":lib:base"),
+                    common: new DepConfig(":lib:common"),
+            ],
+            androidx     : [
+                    core_ktx        : new DepConfig("androidx.core:core-ktx:1.3.2"),
+                    appcompat       : new DepConfig("androidx.appcompat:appcompat:1.2.0"),
+                    multidex        : new DepConfig("androidx.multidex:multidex:2.0.0"),
+                    constraintlayout: new DepConfig("androidx.constraintlayout:constraintlayout:2.0.4"),
+            ],
+            kotlin       : new DepConfig("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"),
+            material     : new DepConfig("com.google.android.material:material:1.2.1"),
+            leakcanary   : [
+                    android         : new DepConfig("com.squareup.leakcanary:leakcanary-android:$leakcanary_version"),
+                    android_no_op   : new DepConfig("com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version"),
+                    support_fragment: new DepConfig("com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version"),
+            ],
+            utilcode     : new DepConfig("com.blankj:utilcodex:1.30.5"),
+            // 混淆
+            free_proguard: new DepConfig("com.blankj:free-proguard:1.0.1"),
+            // 侧边返回
+            swipe_panel  : new DepConfig("com.blankj:swipe-panel:1.1"),
+
+    ]
 }
--- buildSrc/src/main/groovy/ConfigUtils.groovy
+++ buildSrc/src/main/groovy/ConfigUtils.groovy
@@ -60,7 +60,42 @@ class ConfigUtils {
             @Override
             void projectsLoaded(Gradle gradle) {
                 GLog.d("projectsLoaded")
-                _projectsLoaded(gradle)
+
+                //generateDep(gradle)
+
+                gradle.addProjectEvaluationListener(new ProjectEvaluationListener() {
+                    /**
+                     * 执行在各 module 的 build.gradle 之前
+                     * @param project
+                     */
+                    @Override
+                    void beforeEvaluate(Project project) {
+                        //GLog.d("beforeEvaluate")
+                        if (project.subprojects.isEmpty()) {// 定位到具体 project
+                            if (project.name == "app") {
+                                GLog.l(project.toString() + " applies buildApp.gradle")
+                                project.apply {
+                                    from "${project.rootDir.path}/buildApp.gradle"
+                                }
+                            } else {
+                                GLog.l(project.toString() + " applies buildLib.gradle")
+                                project.apply {
+                                    from "${project.rootDir.path}/buildLib.gradle"
+                                }
+                            }
+                        }
+                    }
+
+                    /**
+                     * 执行在各 module 的 build.gradle 之后
+                     * @param project
+                     * @param state
+                     */
+                    @Override
+                    void afterEvaluate(Project project, ProjectState state) {
+                        //GLog.d("afterEvaluate")
+                    }
+                })
             }
 
             /**
@@ -83,42 +118,9 @@ class ConfigUtils {
         })
     }
 
-    private static _projectsLoaded(Gradle gradle) {
-        gradle.addProjectEvaluationListener(new ProjectEvaluationListener() {
-            /**
-             * 执行在各 module 的 build.gradle 之前
-             * @param project
-             */
-            @Override
-            void beforeEvaluate(Project project) {
-                //GLog.d("beforeEvaluate")
-                if (project.subprojects.isEmpty()) {// 定位到具体 project
-                    if (project.name == "app") {
-                        GLog.l(project.toString() + " applies buildApp.gradle")
-                        project.apply {
-                            from "${project.rootDir.path}/buildApp.gradle"
-                        }
-                    } else {
-                        GLog.l(project.toString() + " applies buildLib.gradle")
-                        project.apply {
-                            from "${project.rootDir.path}/buildLib.gradle"
-                        }
-                    }
-                }
-            }
-
-            /**
-             * 执行在各 module 的 build.gradle 之后
-             * @param project
-             * @param state
-             */
-            @Override
-            void afterEvaluate(Project project, ProjectState state) {
-                //GLog.d("afterEvaluate")
-            }
-        })
-    }
-
+    /**
+     * 在 settings.gradle 中 根据 appConfig 和 pkgConfig 来 include 本地模块
+     */
     private static includeModule(Settings settings) {
         /*
         include ':feature:launcher:app'
@@ -131,10 +133,133 @@ class ConfigUtils {
         include ':lib:common'
         include ':lib:base'
          */
+
         settings.include ':lib:base', ':lib:common',
                 ':feature:feature0:export', ':feature:feature1:export',
                 ':feature:feature0:pkg', ':feature:feature1:pkg',
                 ':feature:feature0:app', ':feature:feature1:app', ':feature:launcher:app'
+
+        /*
+        def config = getDepConfigByFilter(new DepConfigFilter() {
+            @Override
+            boolean accept(String name, DepConfig config) {
+                // 如果最终是 app 的话
+                if (name.endsWith('.app')) {
+                    // 获取 app 模块的名字
+                    def appName = name.substring('feature.'.length(), name.length() - 4)
+                    // 如果 Config.appConfig 中不存在，那就不让它进依赖
+                    if (!Config.appConfig.contains(appName)) {
+                        config.isApply = false
+                    }
+                }
+                // 如果 Config.pkgConfig 不为空，说明是 pkg 调试模式
+                if (!Config.pkgConfig.isEmpty()) {
+                    // 如果是 pkg 的话
+                    if (name.endsWith('.pkg')) {
+                        // 获取 pkg 模块的名字
+                        def pkgName = name.substring('feature.'.length(), name.length() - 4)
+                        // 如果 Config.pkgConfig 中不存在，那就不让它进依赖
+                        if (!Config.pkgConfig.contains(pkgName)) {
+                            config.isApply = false
+                        }
+                    }
+                }
+                // 过滤出本地并且 apply 的模块
+                if (!config.isApply) return false
+                if (!config.useLocal) return false
+                if (config.localPath == "") return false
+                return true
+            }
+        }).each { _, cfg ->
+            // 把本地模块 include 进去
+            settings.include cfg.localPath
+        }
+        GLog.l("includeModule = ${GLog.object2String(config)}")
+         */
+    }
+
+    /**
+     * 根据 depConfig 生成 dep
+     */
+    private static generateDep(Gradle gradle) {
+        def config = getDepConfigByFilter(new DepConfigFilter() {
+            @Override
+            boolean accept(String name, DepConfig config) {
+                if (config.useLocal) {
+                    // 如果使用的是本地模块，那么把它转化为 project
+                    config.dep = gradle.rootProject.findProject(config.localPath)
+                } else {
+                    // 如果是远端依赖，那就直接使用远端依赖即可
+                    config.dep = config.remotePath
+                }
+                return true
+            }
+        })
+        GLog.l("generateDep = ${GLog.object2String(config)}")
+    }
+
+    // =============================================================================================
+    // 获取可用的 pkg 和 export
+    // =============================================================================================
+
+    static getApplyPkgs() {
+        def applyPkgs = getDepConfigByFilter(new DepConfigFilter() {
+            @Override
+            boolean accept(String name, DepConfig config) {
+                if (!config.isApply) return false
+                return name.endsWith(".pkg")
+            }
+        })
+        GLog.d("getApplyPkgs = ${GLog.object2String(applyPkgs)}")
+        return applyPkgs
+    }
+
+    static getApplyExports() {
+        def applyExports = getDepConfigByFilter(new DepConfigFilter() {
+            @Override
+            boolean accept(String name, DepConfig config) {
+                if (!config.isApply) return false
+                return name.endsWith(".export")
+            }
+        })
+        GLog.d("getApplyExports = ${GLog.object2String(applyExports)}")
+        return applyExports
+    }
+
+    // =============================================================================================
+    // 获取 DepConfig
+    // =============================================================================================
+
+    /**
+     * 根据过滤器来获取 DepConfig
+     */
+    static Map<String, DepConfig> getDepConfigByFilter(DepConfigFilter filter) {
+        return _getDepConfigByFilter("", Config.depConfig, filter)
+    }
+
+    private static _getDepConfigByFilter(String namePrefix, Map map, DepConfigFilter filter) {
+        // 结果 Map
+        def depConfigList = [:]
+        for (Map.Entry entry : map.entrySet()) {
+            def (name, value) = [entry.getKey(), entry.getValue()]
+            if (value instanceof Map) {
+                // 如果值是 Map 类型就加到结果 Map 中
+                namePrefix += (name + '.')
+                depConfigList.putAll(_getDepConfigByFilter(namePrefix, value, filter))
+                namePrefix -= (name + '.')
+                continue
+            }
+            def config = value as DepConfig
+            if (filter == null || filter.accept(namePrefix + name, config)) {
+                // 符合过滤条件就加到结果 Map 中
+                depConfigList.put(namePrefix + name, config)
+            }
+        }
+        return depConfigList
+    }
+
+    interface DepConfigFilter {
+        boolean accept(String name, DepConfig config);
     }
 
 }
