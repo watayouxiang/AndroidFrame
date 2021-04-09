f84069c26e695c9884c5d626188478caab352993
watayouxiang <watayouxiang@qq.com>
2021-04-09 11:39:04 +0800

    empty

---
+++ feature/feature0/app/src/main/java/com/watayouxiang/feature0/app/Feature0App.java
@@ -0,0 +1,7 @@
+package com.watayouxiang.feature0.app;
+
+
+import com.watayouxiang.common.CommonApplication;
+
+public class Feature0App extends CommonApplication {
+}
---
+++ feature/feature1/app/src/main/java/com/watayouxiang/feature1/app/Feature1App.java
@@ -0,0 +1,7 @@
+package com.watayouxiang.feature1.app;
+
+
+import com.watayouxiang.common.CommonApplication;
+
+public class Feature1App extends CommonApplication {
+}
