bfe5fe801c56b675d350d579c0b12c81cdf4d870
watayouxiang <watayouxiang@qq.com>
2021-04-09 10:59:05 +0800

    empty

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -18,10 +18,13 @@ class Config {
                     core_ktx : "androidx.core:core-ktx:1.3.2",
                     appcompat: "androidx.appcompat:appcompat:1.2.0",
                     multidex : "androidx.multidex:multidex:2.0.0",
+                    constraintlayout : "androidx.constraintlayout:constraintlayout:2.0.4",
             ],
             utilcode: "com.blankj:utilcodex:1.30.5",
             // 混淆
             free_proguard: "com.blankj:free-proguard:1.0.1",
+            // 侧边返回
+            swipe_panel  : "com.blankj:swipe-panel:1.1",
     ]
 
 }
--- lib/common/src/main/java/com/watayouxiang/common/CommonBackActivity.java
+++ lib/common/src/main/java/com/watayouxiang/common/CommonBackActivity.java
@@ -2,14 +2,35 @@ package com.watayouxiang.common;
 
 import android.os.Bundle;
 
+import com.blankj.swipepanel.SwipePanel;
+import com.blankj.utilcode.util.SizeUtils;
 import com.watayouxiang.base.BaseActivity;
 
 
 public abstract class CommonBackActivity extends BaseActivity {
 
+    public abstract boolean isSwipeBack();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initSwipeBack();
+    }
+
+    private void initSwipeBack() {
+        if (isSwipeBack()) {
+            final SwipePanel swipeLayout = new SwipePanel(this);
+            swipeLayout.setLeftDrawable(R.drawable.base_back);
+            swipeLayout.setLeftEdgeSize(SizeUtils.dp2px(100));
+            swipeLayout.wrapView(findViewById(android.R.id.content));
+            swipeLayout.setOnFullSwipeListener(new SwipePanel.OnFullSwipeListener() {
+                @Override
+                public void onFullSwipe(int direction) {
+                    swipeLayout.close(direction);
+                    finish();
+                }
+            });
+        }
     }
 
 }
