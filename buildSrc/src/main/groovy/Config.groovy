71132ff5b89b6e99bb568c9b7fdba44f82170ec9
watayouxiang <watayouxiang@qq.com>
2021-04-12 10:30:53 +0800

    empty

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -6,7 +6,7 @@ class Config {
     static compileSdkVersion = 30
     static minSdkVersion = 16
     static targetSdkVersion = 30
-    static versionCode = 1_000_000
+    static versionCode = 1
     static versionName = '1.0.0'
 
     static kotlin_version = '1.4.32'
