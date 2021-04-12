46c9e7b0e6035650ddf1fe096211fb27acd8da17
watayouxiang <watayouxiang@qq.com>
2021-04-12 15:26:02 +0800

    empty

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -84,6 +84,43 @@ class Config {
             swipe_panel  : new DepConfig("com.blankj:swipe-panel:1.1"),
     ]*/
 
+    /*// appConfig 配置的是可以跑 app 的模块，git 提交务必只包含 launcher
+    static appConfig = ['launcher']
+    // pkgConfig 配置的是要依赖的功能包，为空则依赖全部，git 提交务必为空
+    static pkgConfig = ['feature0']
+    static depConfig = [
+            feature_mock               : new DepConfig(":feature:mock"),
+            feature_launcher_app       : new DepConfig(":feature:launcher:app"),
+            feature_feature0_app       : new DepConfig(":feature:feature0:app"),
+            feature_feature0_pkg       : new DepConfig(":feature:feature0:pkg"),
+            feature_feature0_export    : new DepConfig(":feature:feature0:export"),
+            feature_feature1_app       : new DepConfig(":feature:feature1:app"),
+            feature_feature1_pkg       : new DepConfig(":feature:feature1:pkg"),
+            feature_feature1_export    : new DepConfig(":feature:feature1:export"),
+
+            lib_base                   : new DepConfig(":lib:base"),
+            lib_common                 : new DepConfig(":lib:common"),
+
+            plugin_gradle              : new DepConfig(pluginPath: "com.android.tools.build:gradle:3.6.4"),
+            plugin_kotlin              : new DepConfig(pluginPath: "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"),
+            plugin_api                 : new DepConfig(pluginPath: "com.blankj:api-gradle-plugin:1.2", pluginId: "com.blankj.api"),
+            plugin_bus                 : new DepConfig(pluginPath: "com.blankj:bus-gradle-plugin:2.4", pluginId: "com.blankj.bus"),
+
+            androidx_core_ktx          : new DepConfig("androidx.core:core-ktx:1.3.2"),
+            androidx_appcompat         : new DepConfig("androidx.appcompat:appcompat:1.2.0"),
+            androidx_multidex          : new DepConfig("androidx.multidex:multidex:2.0.0"),
+            androidx_constraintlayout  : new DepConfig("androidx.constraintlayout:constraintlayout:2.0.4"),
+            kotlin                     : new DepConfig("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"),
+            material                   : new DepConfig("com.google.android.material:material:1.2.1"),
+            leakcanary_android         : new DepConfig("com.squareup.leakcanary:leakcanary-android:$leakcanary_version"),
+            leakcanary_android_no_op   : new DepConfig("com.squareup.leakcanary:leakcanary-android-no-op:$leakcanary_version"),
+            leakcanary_support_fragment: new DepConfig("com.squareup.leakcanary:leakcanary-support-fragment:$leakcanary_version"),
+
+            utilcode                   : new DepConfig("com.blankj:utilcodex:1.30.5"),
+            free_proguard              : new DepConfig("com.blankj:free-proguard:1.0.1"),
+            swipe_panel                : new DepConfig("com.blankj:swipe-panel:1.1"),
+    ]*/
+
     // appConfig 配置的是可以跑 app 的模块，git 提交务必只包含 launcher
     static appConfig = ['launcher']
     // pkgConfig 配置的是要依赖的功能包，为空则依赖全部，git 提交务必为空
@@ -102,17 +139,6 @@ class Config {
             lib_base                   : new DepConfig(true, true, ":lib:base"),
             lib_common                 : new DepConfig(true, true, ":lib:common"),
             /*Never delete this line*/
-
-//            feature_mock               : new DepConfig(":feature:mock"),
-//            feature_launcher_app       : new DepConfig(":feature:launcher:app"),
-//            feature_feature0_app       : new DepConfig(":feature:feature0:app"),
-//            feature_feature0_pkg       : new DepConfig(":feature:feature0:pkg"),
-//            feature_feature0_export    : new DepConfig(":feature:feature0:export"),
-//            feature_feature1_app       : new DepConfig(":feature:feature1:app"),
-//            feature_feature1_pkg       : new DepConfig(":feature:feature1:pkg"),
-//            feature_feature1_export    : new DepConfig(":feature:feature1:export"),
-//            lib_base                   : new DepConfig(":lib:base"),
-//            lib_common                 : new DepConfig(":lib:common"),
 
             plugin_gradle              : new DepConfig(pluginPath: "com.android.tools.build:gradle:3.6.4"),
             plugin_kotlin              : new DepConfig(pluginPath: "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"),
